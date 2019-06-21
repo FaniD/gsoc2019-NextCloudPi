31a3e383c03c054a70308502758477eaf1c7db23
FaniD <fani.dimou92@gmail.com>
2019-06-22 00:57:58 +0300

    Update .travis.yml

--- build-SD-rpi.sh
+++ build-SD-rpi.sh
@@ -44,7 +44,7 @@ PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin \
 #PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin \
   #LANG="en_US.UTF-8" LC_CTYPE="en_US.UTF-8" LC_NUMERIC="en_US.UTF-8" LC_TIME="en_US.UTF-8" LC_COLLATE="en_US.UTF-8" LC_MONETARY="en_US.UTF-8" LC_MESSAGES="en_US.UTF-8" LC_PAPER="en_US.UTF-8" LC_NAME="en_US.UTF-8" LC_ADDRESS="en_US.UTF-8" LC_TELEPHONE="en_US.UTF-8" LC_MEASUREMENT="en_US.UTF-8" LC_IDENTIFICATION="en_US.UTF-8" LC_ALL="en_US.UTF-8" sudo chroot raspbian_root /bin/bash <<'EOFCHROOT'
     set -e
-    sudo locale-gen "en_US.UTF-8"  # or your preferred locale
+    echo 'locale-generator' && sudo locale-gen en_US.UTF-8  # or your preferred locale
     # mark the image as an image build
     touch /.ncp-image
 
