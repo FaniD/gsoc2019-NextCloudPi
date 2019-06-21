58e0b19c75e91fb454f15c53a0117755568baf69
FaniD <fani.dimou92@gmail.com>
2019-06-21 23:36:45 +0300

    Update build-SD-rpi.sh
    Include LC vars before running the chroot

--- build-SD-rpi.sh
+++ build-SD-rpi.sh
@@ -40,7 +40,7 @@ mkdir raspbian_root/tmp/ncp-build
 rsync -Aax --exclude-from .gitignore --exclude *.img --exclude *.bz2 . raspbian_root/tmp/ncp-build
 
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin \
-  sudo chroot raspbian_root /bin/bash <<'EOFCHROOT'
+  LANG="en_US.UTF-8" LC_CTYPE="en_US.UTF-8" LC_NUMERIC="en_US.UTF-8" LC_TIME="en_US.UTF-8" LC_COLLATE="en_US.UTF-8" LC_MONETARY="en_US.UTF-8" LC_MESSAGES="en_US.UTF-8" LC_PAPER="en_US.UTF-8" LC_NAME="en_US.UTF-8" LC_ADDRESS="en_US.UTF-8" LC_TELEPHONE="en_US.UTF-8" LC_MEASUREMENT="en_US.UTF-8" LC_IDENTIFICATION="en_US.UTF-8" LC_ALL="en_US.UTF-8" sudo chroot raspbian_root /bin/bash <<'EOFCHROOT'
     set -e
 
     # mark the image as an image build
