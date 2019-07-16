c35aaf8db3a284b6f710ae634c05e531b89d946f
FaniD <fani.dimou92@gmail.com>
2019-07-16 03:14:26 +0300

    Update docker_swarm_create.sh
    Fix volume paths to correspond to the right directory
    of the named volume we mount to the stack

--- docker-swarm/docker_swarm_create.sh
+++ docker-swarm/docker_swarm_create.sh
@@ -24,7 +24,7 @@ docker swarm init --advertise-addr ${leader_IP}
 docker run -it -d -p 5000:8080 -v /var/run/docker.sock:/var/run/docker.sock dockersamples/visualizer
 
 # Set manager to drain so that ncp replicas are distributed to workers
-docker node update --availability drain ${leader_name}
+# docker node update --availability drain ${leader_name}
 
 # Registry option
 #docker service create --name registry --publish published=5001,target=5001 registry:2
@@ -40,17 +40,16 @@ worker_join_token=$(docker swarm join-token -q worker)
 
 # Setup GlusterFS
 
-mkdir /etc/glusterfs
-mkdir /var/lib/glusterd
-mkdir /var/log/glusterfs
-mkdir -p /bricks/brick1/gv0
-mkdir /data
+sudo mkdir /etc/glusterfs
+sudo mkdir /var/lib/glusterd
+sudo mkdir /var/log/glusterfs
+sudo mkdir -p /bricks/brick1/gv0
 docker network create -d overlay --attachable netgfs
 
-mount --bind /data /data
-mount --make-shared /data
+sudo mount --bind /var/lib/docker/volumes/NCP_ncdata/_data /var/lib/docker/volumes/NCP_ncdata/_data
+sudo mount --make-shared /var/lib/docker/volumes/NCP_ncdata/_data
 
-docker run --restart=always --name gfsc0 -v /bricks:/bricks -v /etc/glusterfs:/etc/glusterfs:z -v /var/lib/glusterd:/var/lib/glusterd:z -v /var/log/glusterfs:/var/log/glusterfs:z -v /sys/fs/cgroup:/sys/fs/cgroup:ro --mount type=bind,source=/data,target=/data,bind-propagation=rshared -d --privileged=true --net=netgfs -v /dev/:/dev gluster/gluster-centos
+docker run --restart=always --name gfsc0 -v /bricks:/bricks -v /etc/glusterfs:/etc/glusterfs:z -v /var/lib/glusterd:/var/lib/glusterd:z -v /var/log/glusterfs:/var/log/glusterfs:z -v /sys/fs/cgroup:/sys/fs/cgroup:ro --mount type=bind,source=/var/lib/docker/volumes/NCP_ncdata/_data,target=/var/lib/docker/volumes/NCP_ncdata/_data,bind-propagation=rshared -d --privileged=true --net=netgfs -v /dev/:/dev gluster/gluster-centos
 
 # Create Docker machines with Virtual Box as driver
 replicas_gfs=""
@@ -58,9 +57,11 @@ for(( i=1; i<="$num_workers"; i++)); do
   docker-machine create --driver virtualbox worker${i}
   docker-machine ssh worker${i} "docker swarm join --token ${worker_join_token} ${leader_IP}:2377"
 #  docker-machine ip worker${i}
+### fix this to be done after the deploy
   docker-machine ssh worker${i} "sudo mkdir /data"
   docker-machine ssh worker${i} "sudo mount --bind /data /data"
   docker-machine ssh worker${i} "sudo mount --make-shared /data"
+###
   docker-machine ssh worker${i} "docker run --restart=always --name gfsc${i} -v /bricks:/bricks -v /etc/glusterfs:/etc/glusterfs:z -v /var/lib/glusterd:/var/lib/glusterd:z -v /var/log/glusterfs:/var/log/glusterfs:z -v /sys/fs/cgroup:/sys/fs/cgroup:ro --mount type=bind,source=/data,target=/data,bind-propagation=rshared -d --privileged=true --net=netgfs -v /dev/:/dev gluster/gluster-centos"
 
   # Connect node's gluster container to the gluster cluster
@@ -71,7 +72,7 @@ done
 # Create replicated volume
 docker exec -it gfsc0 gluster volume create gv0 replica ${replicas} ${replicas_gfs}
 docker exec -it gfsc0 gluster volume start gv0
-docker exec -it gfsc0 mount.glusterfs gfsc0:/gv0 /data
+docker exec -it gfsc0 mount.glusterfs gfsc0:/gv0 /var/lib/docker/volumes/NCP_ncdata/_data
 
 # Service ncp start - leader's IP
 #machine_IP=$(docker-machine ip worker1)
--- docker-swarm/gluster_setup.sh
+++ docker-swarm/gluster_setup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-mkdir /etc/glusterfs
-mkdir /var/lib/glusterd
-mkdir /var/log/glusterfs
+sudo mkdir /etc/glusterfs
+sudo mkdir /var/lib/glusterd
+sudo mkdir /var/log/glusterfs
 
-sudo mkdir /data
-sudo mount --bind /data /data
-sudo mount --make-shared /data
+# sudo mkdir /var/lib/docker/volumes/NCP_ncdata/_data/
+sudo mount --bind /var/lib/docker/volumes/NCP5_ncdata/_data /var/lib/docker/volumes/NCP5_ncdata/_data
+sudo mount --make-shared /var/lib/docker/volumes/NCP5_ncdata/_data
 
 sudo mkdir -p /bricks/brick1/gv0
 
-docker run --restart=always --name gfsc0 -v /bricks:/bricks -v /etc/glusterfs:/etc/glusterfs:z -v /var/lib/glusterd:/var/lib/glusterd:z -v /var/log/glusterfs:/var/log/glusterfs:z -v /sys/fs/cgroup:/sys/fs/cgroup:ro --mount type=bind,source=/data,target=/data,bind-propagation=rshared -d --privileged=true --net=netgfs -v /dev/:/dev gluster/gluster-centos
+docker run --restart=always --name gfsc0 -v /bricks:/bricks -v /etc/glusterfs:/etc/glusterfs:z -v /var/lib/glusterd:/var/lib/glusterd:z -v /var/log/glusterfs:/var/log/glusterfs:z -v /sys/fs/cgroup:/sys/fs/cgroup:ro --mount type=bind,source=/var/lib/docker/volumes/NCP5_ncdata/_data,target=/var/lib/docker/volumes/NCP5_ncdata/_data,bind-propagation=rshared -d --privileged=true --net=netgfs -v /dev/:/dev gluster/gluster-centos
