8ac583042494f26e2371b166a92dbdfc71495160
FaniD <fani.dimou92@gmail.com>
2019-08-12 11:52:57 +0300

    Run again SecureWilly

--- SecureWilly/Parser/dynamic_scripts/7_run.sh
+++ SecureWilly/Parser/dynamic_scripts/7_run.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
  
 IP=$(ip route get 8.8.8.8 | awk -F"src " 'NR==1{split($2,a," ");print a[1]}')
-echo "${IP}"
-docker run -d -t -p 4443:4443 -p 443:443 -p 80:80 -v ncdata:/data --name nextcloudpi ownyourbits/nextcloudpi-x86:latest ${IP}
+docker run -d -t -p 4443:4443 -p 443:443 -p 80:80 -v ncdata:/data --security-opt "apparmor=nextcloudpi_profile" --name nextcloudpi nextcloudpi $IP
+echo "Activation page . . ."
+sleep 60
 export MOZ_HEADLESS=1
 sleep 60
 ./../../tests/activation_tests.py ${IP}
-sleep 60
-./../../tests/nextcloud_tests.py ${IP}
-sleep 5
-./../../tests/system_tests.py ncp@${IP}
+sudo touch /var/lib/docker/volumes/ncdata/_data/nextcloud/data/ncp/files/hello_world
+docker exec nextcloudpi /usr/local/bin/ncp-scan
 docker kill nextcloudpi
+docker rm nextcloudpi
+docker volume rm ncdata
