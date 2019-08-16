5d615f197b178312b4fd1bc80ec0fcccd064b03e
FaniD <fani.dimou92@gmail.com>
2019-08-16 11:20:34 +0300

    Update create_vm.sh

--- Vagrant/create_vm.sh
+++ Vagrant/create_vm.sh
@@ -29,7 +29,7 @@ function reserved_ip() {
   return $stat
 }
 
-echo -e "===================================\n"
+echo -e "\n===================================\n"
 while true; do 
   echo -e "Please enter number (1/2):"
   echo -e "(1) for new VM"
@@ -42,7 +42,7 @@ while true; do
   fi 
 done
 
-echo -e "===================================\n"
+echo -e "\n===================================\n"
 while true; do 
   echo -e "Specify prefered way of networking:"
   echo -e "(1) Public network (Bridged Networking)\n\tThe IP of new VM will be visible to everyone inside the LAN"
@@ -55,7 +55,7 @@ while true; do
   fi
 done
 
-echo -e "==================================\n"
+echo -e "\n==================================\n"
 while true; do 
   echo -e "Please choose between using a specific IP\nor picking any IP available?"
   echo -e "Type either the IP within the LAN you want to use or 'any' (<IP>/any):"
@@ -75,12 +75,13 @@ else
   vm_dir="NCP_VM_clone"
 fi 
 
-host_interface=$(ip addr | grep 192.168.1.18 | cut -d' ' -f13)
+host_ip=$(ip route get 8.8.8.8 | awk -F"src " 'NR==1{split($2,a," ");print a[1]}')
+host_interface=$(ip addr | grep ${host_ip} | cut -d' ' -f13)
 
 mkdir -p ${vm_dir}
 touch ${vm_dir}/Vagrantfile
 
-cat << 'EOF' > ${vm_dir}/Vagrantfile
+cat <<EOF > ${vm_dir}/Vagrantfile
 # -*- mode: ruby -*-
 # vi: set ft=ruby :
 
@@ -107,12 +108,12 @@ Vagrant.configure("2") do |config|
   #Networking
 
   #Public IP
-  
-  config.vm.network "public_network", bridge: "${host_interface}", ip: "${IP}"
+  #config.vm.network "public_network", bridge: "${host_interface}"
+  #config.vm.network "public_network", bridge: "${host_interface}", ip: "${IP}"
 
   #Private IP
-  #config.vm.network "private_network", ip: "192.168.50.4"
   #config.vm.network "private_network", type: "dhcp"
+  #config.vm.network "private_network", ip: "${IP}"
 
   #Provider settings
   config.vm.provider "virtualbox" do |v|
@@ -130,8 +131,8 @@ Vagrant.configure("2") do |config|
 
   $script = <<-SHELL
     sudo su
-    BRANCH=gsoc2019-master
-    #BRANCH=gsoc2019-devel  # uncomment to install devel
+    BRANCH=master
+    #BRANCH=devel  # uncomment to install devel
     apt-get update
     DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends git
 
@@ -139,11 +140,11 @@ Vagrant.configure("2") do |config|
     touch /.ncp-image
 
     # install
-    git clone -b "$BRANCH" https://github.com/eellak/gsoc2019-NextCloudPi.git /tmp/nextcloudpi
+    git clone -b "$BRANCH" https://github.com/nextcloud/nextcloudpi.git /tmp/nextcloudpi
     cd /tmp/nextcloudpi
 
     # uncomment to install devel
-    #sed -i 's|^BRANCH=gsoc2019-master|BRANCH=gsoc2019-devel|' install.sh ncp.sh
+    #sed -i 's|^BRANCH=master|BRANCH=devel|' install.sh ncp.sh
 
     bash install.sh
 
@@ -162,33 +163,46 @@ Vagrant.configure("2") do |config|
 end
 EOF
 
-[[ ($choice == "1") ]] && {
-  #Setup origin VM, according to the existing Vagrantfile
-#  mkdir NCP_VM
+sed -i '49s,=,script =,' ${vm_dir}/Vagrantfile
 
+if [[ $network == "1" ]]; then
+  if [[ $IP == "any" ]]; then
+    sed -i '27s,#,,' ${vm_dir}/Vagrantfile
+  else
+    sed -i '28s,#,,' ${vm_dir}/Vagrantfile
+  fi
+else
+  if [[ $IP == "any" ]]; then
+    sed -i '31s,#,,' ${vm_dir}/Vagrantfile
+  else
+    sed -i '32s,#,,' ${vm_dir}/Vagrantfile
+  fi
+fi
+
+[[ ($choice == "1") ]] && {   
+  #Setup origin VM, according to the existing Vagrantfile
   cd NCP_VM
-#  vagrant up
-  echo -e "\nYour NCP VM is ready. Type localhost:8000 to your web browser to activate it."
-  return 0
+  vagrant up
+  if [[ ${IP} == any ]]; then
+    IP=$(vagrant ssh -c "ip route get 8.8.8.8 | awk -F"src " 'NR==1{split($2,a," ");print a[1]}")
+  fi
+  echo -e "\nYour NCP VM is ready. Type localhost:${IP} to your web browser to activate it."
 }
 
 [[ ($choice == "2") ]] && {
+  cd NCP_VM
   #Turn off parent VM
   vagrant halt 
   #Export a box from the parent VM
   vagrant package
 
   #Create new dir and new Vagrantfile - comment out every command in Vagranfile's script except for power off
-  mkdir NCP_VM_clone
-  cd NCP_VM_clone
-  cp ../Vagrantfile .
-  sed -i 's,v.name = "NextCloudPi",v.name = "NextcloudPi_clone",' Vagrantfile
-  sed -i 's,config.vm.box = "debian/stretch64",config.vm.box = "../package.box",' Vagrantfile
-  sed -i '50,74s/^#*/#/' Vagrantfile
-  sed -i '75s/#//' Vagrantfile
+  cd ../NCP_VM_clone
+  sed -i 's,v.name = "NextCloudPi",v.name = "NextcloudPi_clone",' ${vm_dir}/Vagrantfile
+  sed -i 's,config.vm.box = "debian/stretch64",config.vm.box = "../NCP_VM/package.box",' ${vm_dir}/Vagrantfile
+  sed -i '49,72s/^#*/#/' ${vm_dir}/Vagrantfile
+  sed -i '73s/#//' ${vm_dir}/Vagrantfile
 
   #Setup the new cloned VM
-  #vagrant up
-
-  return 0
+  vagrant up
 }
